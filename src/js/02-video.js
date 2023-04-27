b6e80a1942ee4481e6b8b25f721e94844786b279
Anton Volvenko <volvenko.anthony@gmail.com>
2023-04-28 00:29:37 +0300

    Added comments to video & feedback parts

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,27 +1,29 @@
+//! Importing the Vimeo Player library and the Lodash throttle function
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
+//! Getting the Vimeo player iframe and create a new player instance
 const iframe = document.querySelector('#vimeo-player');
 const player = new Player(iframe);
 
+//* Set a key for the current time in the player
 const currentTimeKey = 'videoplayer-current-time';
 
-// Оновлюємо локальне сховище з поточним часом відтворення
+//* Save the current time to local storage every second using throttling
 const saveCurrentTime = throttle(() => {
   player.getCurrentTime().then(time => {
     localStorage.setItem(currentTimeKey, time);
   });
 }, 1000);
 
-// Відновлюємо відтворення зі збереженої позиції
+//* Restore the video playback from the saved position
 const restoreCurrentTime = () => {
   const currentTime = localStorage.getItem(currentTimeKey);
-
   if (currentTime) {
     player.setCurrentTime(currentTime);
   }
 };
 
+//* Listen for the 'timeupdate' event and save the current time && restore the saved playback position
 player.on('timeupdate', saveCurrentTime);
-
 restoreCurrentTime();
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,12 @@
-import throttle from 'lodash.throttle';
-
+//! Selecting form and input fields
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
+
+//! Seting key for local storage due to the task requirements
 const storageKey = 'feedback-form-state';
 
+//* Saving form input values to local storage
 const saveToLocalStorage = () => {
   const feedbackFormState = {
     email: emailInput.value,
@@ -13,6 +15,7 @@ const saveToLocalStorage = () => {
   localStorage.setItem(storageKey, JSON.stringify(feedbackFormState));
 };
 
+//* Updating form fields with values from local storage
 const updateFormFields = () => {
   const feedbackFormState = JSON.parse(localStorage.getItem(storageKey));
   if (feedbackFormState) {
@@ -21,6 +24,7 @@ const updateFormFields = () => {
   }
 };
 
+//! Clearing form and local storage
 const clearFormAndLocalStorage = () => {
   const feedbackFormState = {
     email: '',
@@ -32,16 +36,20 @@ const clearFormAndLocalStorage = () => {
   console.log(feedbackFormState);
 };
 
+//* Save form input values to local storage using lodash throttle
 const throttledSaveToLocalStorage = throttle(saveToLocalStorage, 500);
 
+// Add event listener to form inputs
 form.addEventListener('input', () => {
   throttledSaveToLocalStorage();
 });
 
+// Update form fields with values from local storage on page load
 window.addEventListener('load', () => {
   updateFormFields();
 });
 
+// Prevent default form submit behavior and clear form and local storage
 form.addEventListener('submit', event => {
   event.preventDefault();
   clearFormAndLocalStorage();
