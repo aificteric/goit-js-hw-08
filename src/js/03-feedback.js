17f7a95c3a09ac73801b9b22490ab45695cd10d9
Anton Volvenko <volvenko.anthony@gmail.com>
2023-04-30 12:07:58 +0300

    Final push

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,10 +1,13 @@
-// Importing the Lodash throttle function
+//! Importing the Lodash throttle function
 import throttle from 'lodash.throttle';
 
+//! Get the form and its inputs and submit button
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 const submitButton = form.querySelector('button[type="submit"]');
+
+//* Define a function that saves the form state to local storage using Lodash throttle
 
 const saveFormState = throttle(() => {
   const formState = {
@@ -14,7 +17,10 @@ const saveFormState = throttle(() => {
   localStorage.setItem('form-state', JSON.stringify(formState));
 }, 500);
 
+//* Add an input event listener to the form that calls the saveFormState function
 form.addEventListener('input', saveFormState);
+
+//* On page load, check if there is saved form state in local storage
 
 window.addEventListener('load', () => {
   const formState = JSON.parse(localStorage.getItem('form-state'));
@@ -24,6 +30,7 @@ window.addEventListener('load', () => {
   }
 });
 
+//* Adding a click event listener to the submit button that validates the form and saves its state to local storage if valid
 submitButton.addEventListener('click', event => {
   event.preventDefault();
   const emailValue = emailInput.value;
