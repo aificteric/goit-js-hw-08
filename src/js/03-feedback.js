92a4690d6c08fa4da86ee3ec483c307d3ab4dd61
Anton Volvenko <volvenko.anthony@gmail.com>
2023-04-30 12:04:08 +0300

    Intermediate push

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,59 +1,43 @@
-//! Importing the Lodash throttle function
+// Importing the Lodash throttle function
 import throttle from 'lodash.throttle';
 
-//! Selecting form and input fields
 const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
+const submitButton = form.querySelector('button[type="submit"]');
 
-//! Seting key for local storage due to the task requirements
-const storageKey = 'feedback-form-state';
-
-//* Saving form input values to local storage
-const saveToLocalStorage = () => {
-  const feedbackFormState = {
+const saveFormState = throttle(() => {
+  const formState = {
     email: emailInput.value,
     message: messageInput.value,
   };
-  localStorage.setItem(storageKey, JSON.stringify(feedbackFormState));
-};
+  localStorage.setItem('form-state', JSON.stringify(formState));
+}, 500);
 
-//* Updating form fields with values from local storage
-const updateFormFields = () => {
-  const feedbackFormState = JSON.parse(localStorage.getItem(storageKey));
-  if (feedbackFormState) {
-    emailInput.value = feedbackFormState.email || '';
-    messageInput.value = feedbackFormState.message || '';
-  }
-};
+form.addEventListener('input', saveFormState);
 
-//! Clearing form and local storage
-const clearFormAndLocalStorage = () => {
-  const feedbackFormState = {
-    email: '',
-    message: '',
-  };
-  localStorage.removeItem(storageKey);
-  emailInput.value = '';
-  messageInput.value = '';
-  console.log(feedbackFormState);
-};
-
-//* Save form input values to local storage using lodash throttle
-const throttledSaveToLocalStorage = throttle(saveToLocalStorage, 500);
-
-// Add event listener to form inputs
-form.addEventListener('input', () => {
-  throttledSaveToLocalStorage();
-});
-
-// Update form fields with values from local storage on page load
 window.addEventListener('load', () => {
-  updateFormFields();
+  const formState = JSON.parse(localStorage.getItem('form-state'));
+  if (formState) {
+    emailInput.value = formState.email || '';
+    messageInput.value = formState.message || '';
+  }
 });
 
-// Prevent default form submit behavior and clear form and local storage
-form.addEventListener('submit', event => {
+submitButton.addEventListener('click', event => {
   event.preventDefault();
-  clearFormAndLocalStorage();
+  const emailValue = emailInput.value;
+  const messageValue = messageInput.value;
+  if (emailValue && messageValue) {
+    const formState = {
+      email: emailValue,
+      message: messageValue,
+    };
+    console.log(formState);
+    localStorage.removeItem('form-state');
+    emailInput.value = '';
+    messageInput.value = '';
+  } else {
+    alert('Please enter your email and message.');
+  }
 });
